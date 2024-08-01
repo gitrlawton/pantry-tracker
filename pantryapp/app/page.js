f1a8daa782ef57bc6b5664130bc427c8f10b672b
Neal L <rlawton.cs@gmail.com>
2024-08-01 11:39:43 -0700

    Finished base design with button to add and remove items.

--- pantryapp/app/page.js
+++ pantryapp/app/page.js
@@ -1,8 +1,8 @@
 'use client'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Modal, Stack, Typography, TextField, Button } from '@mui/material'
 import { useState, useEffect } from "react"
 import {firestore} from "../firebase"
-import { collection, getDocs, query } from "firebase/firestore"
+import { collection, deleteDoc, doc, getDocs, getDoc, setDoc, query } from "firebase/firestore"
 
 const items = ["tomato", "potato", "onion", "garlic", "ginger", "carrot"]
 
@@ -50,7 +50,7 @@ const addItem = async (item) => {
   if (docSnap.exists()) {
     // Get the quantity of the item from the data.
     const {quantity} = docSnap.data()
-    await setDoc(docRef, {quantity: quantity - 1})
+    await setDoc(docRef, {quantity: quantity + 1})
     }
   else {
     await setDoc(docRef, {quantity: 1})
@@ -86,34 +86,90 @@ useEffect(() => {
   updateInventory()
 }, [])
 
-// Model helper functions. //
+// Modal helper functions. //
+
+
 const handleOpen = () => setOpen(true)
+
+// Closes our database after updating it.
 const handleClose = () => setOpen(false)
 
   return (
     <Box 
       width="100vw"
       height="100vh"
-      display={"flex"}
-      flexDirection={"column"}
-      justifyContent={"center"}
-      alignItems={"center"}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
     >
-      <Box 
-        border={"1px solid #333"}
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack
+            width="100%"
+            direction="row"
+            spacing={2} 
+          >
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value)
+              }}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName)
+                setItemName("")
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button
+        variant="contained"
+        onClick={() => {
+          handleOpen()
+        }}
+      >
+        Add New Item
+      </Button>
+      <Box
+        border="1px solid #333"
       >
         <Box
           width="800px"
           height="100px"
-          bgcolor={"#ADD8E6"}
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
+          bgcolor="#ADD8E6"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
         >
-          <Typography
-            variant={"h2"}
-            color={"#333"}
-            textAlign={"center"}
+          <Typography 
+            variant="h2"
+            color="#333"
           >
             Pantry Items
           </Typography>
@@ -122,30 +178,57 @@ const handleClose = () => setOpen(false)
           width="800px"
           height="300px"
           spacing={2}
-          overflow={"auto"}
+          overflow="auto"
         >
-          {items.map((i) => (
-            <Box
-              key={i}
-              width="100%"
-              height="300px"
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              bgcolor={"#f0f0f0"}
-            >
-              <Typography
-                variant={"h3"}
-                color={"#333"}
-                textAlign={"center"}
+          {
+            inventory.map(({name, quantity}) => (
+              <Box 
+                key={name}
+                width="100%"
+                minHeight="150px" 
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                bgcolor="#f0f0f0"
+                padding={5}
               >
-                { i.charAt(0).toUpperCase() + i.slice(1) }
-              </Typography>
-            </Box>
-          ))}
-
+                <Typography
+                  variant="h3"
+                  color="#333"
+                  textAlign="center"
+                >
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </Typography>
+                <Typography
+                  variant="h3"
+                  color="#333"
+                  textAlign="center"
+                >
+                  {quantity}
+                </Typography>
+                <Stack direction="row" spacing={2}>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      addItem(name)
+                    }}
+                  >
+                    Add
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      removeItem(name)
+                    }}
+                  >
+                    Remove
+                  </Button>
+                </Stack>
+              </Box>
+            ))
+          }
         </Stack>
-      </Box>
+      </Box>  
     </Box>
   );
 }
